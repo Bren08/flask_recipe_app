a4df6fecf2b7bc9254538abc57b6e824a68fc02d
brendan o brien <brenande@gmail.com>
2019-08-02 01:00:28 +0100

    Adjusted the layout with css styling and spacing

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import TEXT
+from config import BaseConfig
 
 app = Flask(__name__)
 
